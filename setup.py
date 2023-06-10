8c94996fb75c34cc1835c81cb8a975a855ae074e
iisriscanner <iisriscanner@gmail.com>
2023-06-10 21:27:58 +1000

    Fix version number.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from pathlib import Path
 
 setuptools.setup(
     name='video_api',
-    version='1.0.',
+    version='1.0.0',
     author='Leo Saif',
     description='Genarate frames from video',
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",
-    url='',
+    url='https://github.com/iisriscanner/video-api',
     license='MIT',
     packages=['video_api'],
     install_requires=['opencv-python'],
