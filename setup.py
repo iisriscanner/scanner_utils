545c3ca4af39d4fe7abbf0e016594bd535781647
iisriscanner <iisriscanner@gmail.com>
2023-06-11 11:19:24 +1000

    Update project name.

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 import setuptools
 from pathlib import Path
 
+NAME = "scanner_utils"
+
 setuptools.setup(
-    name='scanner_api',
-    version='1.1.0',
-    author='Leo Saif',
-    description='Genarate frames from video',
+    name=NAME,
+    version="1.1.0",
+    author="Leo Saif",
+    description="Genarate frames from video",
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",
-    url='https://github.com/iisriscanner/scanner_utils',
-    license='MIT',
-    packages=['scanner_utils'],
-    install_requires=['opencv-python'],
+    url=f"https://github.com/iisriscanner/{NAME}",
+    license="MIT",
+    packages=[f"{NAME}"],
+    install_requires=["opencv-python"],
 )
